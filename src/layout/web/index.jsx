a10557ec307df62c01dee89848b76fec6bebf3e7
TAL <1106408264@qq.com>
2020-11-16 23:02:46 +0800

    📝 路由初始化props对象并抛给将要打开的组件

--- src/layout/web/index.jsx
+++ src/layout/web/index.jsx
@@ -10,7 +10,12 @@ import AppMain from './AppMain'
 const siderLayout = { xxl: 4, xl: 5, lg: 5, sm: 0, xs: 0 }
 const contentLayout = { xxl: 20, xl: 19, lg: 19, sm: 24, xs: 24 }
 
+// layout组件负责整个页面的布局
+// 为react组件开发的常见用法
 const WebLayout = props => {
+
+  // 路由投放的组件将自动携带参数到props
+  console.log(props)
   return (
     <Layout className='app-container'>
       <Header />
