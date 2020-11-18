f68cdb222241f71c5df2d783777d9045544c75ac
TAL <1106408264@qq.com>
2020-11-18 23:55:54 +0800

    📝 dangerouslySetInnerHTML为react的一个标签属性，可以插入一个html片段

--- src/views/web/home/List.jsx
+++ src/views/web/home/List.jsx
@@ -7,28 +7,36 @@ import { Divider } from 'antd'
 import SvgIcon from '@/components/SvgIcon'
 import ArticleTag from '@/components/ArticleTag'
 
+// react子组件竟然可以使用父组件的样式
+
 const ArticleList = props => {
   const history = useHistory()
   const { list } = props
 
+  // 使用history进入对应的文章
   function jumpTo(id) {
     history.push(`/article/${id}`)
   }
 
   return (
+    // app-home-list使用父组件的样式
     <ul className='app-home-list'>
       {list.map(item => (
         <li key={item.id} className='app-home-list-item'>
+          {/* 文章标题 */}
           <Divider orientation='left'>
+            {/* 文章标题 */}
             <span className='title' onClick={() => jumpTo(item.id)}>
               {item.title}
             </span>
+            {/* 文章日期 */}
             <span className='posted-time'>{item.createdAt.slice(0, 10)}</span>
           </Divider>
 
           <div
             onClick={() => jumpTo(item.id)}
             className='article-detail content'
+            // dangerouslySetInnerHTML为react的一个标签属性，可以插入html片段
             dangerouslySetInnerHTML={{ __html: item.content }}
           />
 
