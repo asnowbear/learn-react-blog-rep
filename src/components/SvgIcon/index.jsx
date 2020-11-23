69e8d4d5e66eb0e36fddff378ff3009292e26273
TAL <1106408264@qq.com>
2020-11-23 23:28:41 +0800

    📝 SvgIcon组件的描述，对于className和style的支持方式

--- src/components/SvgIcon/index.jsx
+++ src/components/SvgIcon/index.jsx
@@ -3,8 +3,10 @@ import PropTypes from 'prop-types'
 
 // iconfont svg
 // function模式创建react组件
+// 定义一个SvgIcon组件
 const SvgIcon = props => {
   return (
+    // className方式和style方式都支持
     <svg className={`svg-icon ${props.className}`} aria-hidden='true' style={props.style}>
       <use xlinkHref={`#${props.type}`} />
     </svg>
