09a3192b25df4c11cc87082c194affd1e4e02377
TAL <1106408264@qq.com>
2020-11-25 17:13:35 +0800

    🎨 quicklink快速导航相关解读

--- src/views/web/home/QuickLink.jsx
+++ src/views/web/home/QuickLink.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 import { useMediaQuery } from 'react-responsive'
 
 // components
+// 特别好的导入方式 import { default as Icon } from '地址'
 import { Icon, Divider, Empty, Drawer, Tag, Spin } from 'antd'
 
 const title = '快速导航'
@@ -15,6 +16,7 @@ const List = props => {
       {showTitle && <Divider>{title}</Divider>}
       {list.map(item => (
         <li key={item.id}>
+          {/* 跳入到指定的路由链接 */}
           <Link to={`/article/${item.id}`}>{item.title}</Link>
         </li>
       ))}
@@ -26,16 +28,21 @@ const List = props => {
  * article quick link
  */
 const QuickLink = props => {
+  // useMediaQuery主要是用来计算当前窗口<1300px
   const isGreaterThan1300 = useMediaQuery({ query: '(min-width: 1300px)' })
   const { list } = props
 
+  // useState还是需要和UI关联的
   const [drawerVisible, setDrawerVisible] = useState(false)
 
+  // 判断当前窗口大小，<1300 显示自定义列表 ：显示抽屉效果（点开显示导航列表）
   return isGreaterThan1300 ? <List list={list} /> : (
     <>
+      {/* 图标 */}
       <div className='drawer-btn' onClick={e => setDrawerVisible(true)}>
         <Icon type='menu-o' className='nav-phone-icon' />
       </div>
+      {/* 抽屉盒子 */}
       <Drawer
         title={title}
         placement='right'
